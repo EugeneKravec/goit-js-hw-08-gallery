e9011d63bcb2d820f8a23086f973d433f915ceec
Eugene Kravec <krava255@gmail.com>
2021-09-20 17:39:19 +0300

    makes rendering of images

---
+++ index.js
@@ -0,0 +1,46 @@
+import { galleryItems } from "./app.js";
+
+// const { preview, original, description } = galleryItems
+const imagesList = document.querySelector('.gallery')
+const lightbox = document.querySelector('.js-lightbox')
+const overlay = document.querySelector('.lightbox__overlay')
+const lightboxImage = document.querySelector('.lightbox__image')
+const closeBtn = document.querySelector('[data-action="close-lightbox"]')
+
+const createGalery = galleryItems.reduce((acc, elem) => {
+    return acc + `<li class="gallery__item">
+  <a class="gallery__link" href=${elem.original}
+  >
+    <img
+      class="gallery__image"
+      src=${elem.preview}
+      data-source=${elem.original}
+      alt=${elem.description}
+    />
+  </a>
+</li>`
+}, '')
+
+imagesList.insertAdjacentHTML('beforeend', createGalery)
+
+imagesList.addEventListener('click', clickOnImage)
+
+function clickOnImage(event) {
+    event.preventDefault();
+    const galleryImage = event.target.classList.contains('gallery__image')
+    if (!galleryImage) {
+        return
+    }
+
+    openLightbox(event.target)
+}
+
+function openLightbox(img) {
+    lightbox.classList.add('is-open')
+    lightboxImage.src = img.dataset.sourse;
+    lightboxImage.dataset.index =
+}
+
+function closeLightbox() {
+
+}
