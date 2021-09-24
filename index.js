618f9d020036382c96c63c283b57152177966c2f
Eugene Kravec <krava255@gmail.com>
2021-09-24 22:05:08 +0300

    Done

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import { galleryItems } from "./app.js";
 
 // const { preview, original, description } = galleryItems
-const imagesList = document.querySelector('.gallery')
+const imagesList = document.querySelector('.js-gallery')
 const lightbox = document.querySelector('.js-lightbox')
 const overlay = document.querySelector('.lightbox__overlay')
 const lightboxImage = document.querySelector('.lightbox__image')
 const closeBtn = document.querySelector('[data-action="close-lightbox"]')
 
 const createGalery = galleryItems.reduce((acc, elem) => {
-    return acc + `<li class="gallery__item">
+  return acc + `<li class="gallery__item">
   <a class="gallery__link" href=${elem.original}
   >
     <img
@@ -26,21 +26,18 @@ imagesList.insertAdjacentHTML('beforeend', createGalery)
 imagesList.addEventListener('click', clickOnImage)
 
 function clickOnImage(event) {
-    event.preventDefault();
-    const galleryImage = event.target.classList.contains('gallery__image')
-    if (!galleryImage) {
-        return
-    }
-
-    openLightbox(event.target)
+  event.preventDefault();
+  const galleryImage = event.target.classList.contains('gallery__image')
+  if (!galleryImage) {
+    return
+  }
+  lightboxImage.src = event.target.dataset.source;
+  lightbox.classList.add('is-open')
 }
 
-function openLightbox(img) {
-    lightbox.classList.add('is-open')
-    lightboxImage.src = img.dataset.sourse;
-    lightboxImage.dataset.index =
-}
+closeBtn.addEventListener('click', closeLightbox)
 
 function closeLightbox() {
-
+  lightboxImage.src = '';
+  lightbox.classList.remove('is-open')
 }
